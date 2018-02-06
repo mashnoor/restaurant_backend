3d4a084a4804693c886669f033a623fe3f9388f7
Nowfel Mashnoor <nmmashnoor@gmail.com>
2018-02-06 12:18:53 +0600

    Experimenting :

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -69,7 +69,7 @@ class ReportsController extends Controller
             ->orderBy('id','desc')
             ->get();
 
-    $mpdf = new \Mpdf\Mpdf(['tempDir' => __DIR__ . '/tmp']);
+    $mpdf = new \Mpdf\Mpdf(['tempDir' => '/var/www/html/tmp']);
     $mpdf->WriteHTML(view('report.reportPdf')->withReports($reports));    
     $mpdf->Output('reports.pdf', 'I');
 
