2ff1446ac7960e77bfb62c095299e20e01911b31
Nowfel Mashnoor <nmmashnoor@gmail.com>
2018-02-07 16:31:10 +0600

    Added Notification Support Again

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use Session;
 use App\Order;
+use App\User;
+use App\Table;
 
 class OrderController extends Controller
 {
@@ -45,6 +47,21 @@ Session::put('ordercnter',count($orders));
     $order->save();
 
     Session::flash('success', 'The Order was successfully Serve!');
+	
+	$user = User::find($order->user_id);
+	$reg_ids = array();
+	array_push($reg_ids, $user->token);
+	$order_res = array();
+	$table = Table::find($order->table_id);
+	
+	$order_res['order_id'] = $id;
+	$order_res['table_code'] = $table->code;
+	
+	
+
+	
+	$this->sendMessage($reg_ids, $order_res);
+	
 
     return redirect()->back();
   }
@@ -61,6 +78,43 @@ Session::put('ordercnter',count($orders));
 
     return redirect()->back();
   }
+   public static function sendMessage($registrationIds, $post)
+    {
+        $firebase_api_key = "AAAAP_zysBA:APA91bFvp-X4wESqrIogi2hK5QlRfS22W_B4wpf3vfvkQ9tLjB14GFJD-ZI2csJeecvr0jfsNXZ4ypeV003l9g4l_zIzB-cwiOg8SamrMUixAYT0oRC-WZGVUjhaOYwLSk7EIc3wGt7N";
+
+
+        $msg = array
+        (
+            'body' => $post
+
+        );
+
+        $fields = array
+        (
+            'registration_ids' => $registrationIds,
+            'data' => $msg
+        );
+
+        $headers = array
+        (
+            'Authorization: key=' . $firebase_api_key,
+            'Content-Type: application/json'
+        );
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://android.googleapis.com/gcm/send');
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+        $result = curl_exec($ch);
+        curl_close($ch);
+
+        return $result;
+
+    }
+  
 
 
 }
